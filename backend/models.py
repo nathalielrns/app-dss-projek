32fc5fdeac6b06bb43738432a560ed872861d7a2
Zahra <zahrahanisya5270@gmail.com>
2026-06-30 12:25:41 +0700

    Update line 4 models.py

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, Text
 from sqlalchemy.orm import relationship
 from datetime import datetime
-from database import Base
+from backend.database import Base
 
 
 class StudiKasus(Base):
